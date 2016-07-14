5df2b07c39365fb9a5bcc7c95fc50d1458d6c419
1n5aN1aC <joshua@mailmag.net>
2016-07-13 17:01:54 -0700

    Add support for ground station pings

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -3,7 +3,7 @@
 
 // Baud rate settings
 #define GPS_BAUD 9600
-#define TX_BAUD 38400
+#define TX_BAUD 57600
 #define ACTUAL_TX_BAUD 1200
 #define TARGET_TX_BAUD 1000
 
@@ -22,6 +22,7 @@ TinyGPSCustom VDOP   (gps, "GPGSA", 18); // Vertical dillution Of Presicion
 //Time counters
 unsigned long lastTelemetryTX = 0UL;
 unsigned long lastStatusTX    = 0UL;
+unsigned long lastRX          = 0UL;
 
 //counters for last packet statistics send
 unsigned int lastBadSum  = 0;
@@ -32,6 +33,7 @@ unsigned int lastWithFix = 0;
 //
 // Run Initial setup Tasks
 //
+// 0. Initialize serial buffers
 // 1. Send Version information
 //
 void setup() {
@@ -51,17 +53,28 @@ void setup() {
 // Main Loop Tasks
 //
 // 1. Recieve & Process GPS data
-// 2. Send Telemetry if needed
-// 3. Send Status information if needed.
+// 2. Recieve & Process Ground Control Packets
+// 3. Send Telemetry if needed
+// 4. Send Status information if needed.
 //
 void loop() {
+  //If there is any new GPS information, read & process it
   while (Serial1.available() > 0) {
     gps.encode(Serial1.read());
   }
 
+  //If we have received a character from ground control, process it.
+  if (Serial.available() )
+  {
+     ch = Serial.read();
+	 process_command(ch);
+  }
+
+  //If we need to send a new telemetry packet, do so.
   if (millis() - lastTelemetryTX > TELEMETRY_FREQUENCY)
     telemetryTX();
 
+  //If we need to send a new status packet, do so.
   if (millis() - lastStatusTX > STATUS_FREQUENCY)
     statusTX();
 }
@@ -100,19 +113,20 @@ void telemetryTX() {
 //
 // Status update Packet:
 // 
-// S,21594500,00000,6,165,165,532,2/1/0
+// S,21594500,000000,6,165,165,532,2/1/0
 // S = This is a status packet
 //   21594500 = GPS time
-//            0 = Time validity byte (0-9 = seconds since update.  X=invalid / >10 seconds)
-//             0 = Location validity byte
-//              0 = Altitude validity byte
-//               0 = Speed validity byte
-//                0 = Course validity byte
-//                  6 = Satellites used for fix
-//                    165 = HDOP (Horrizontal dillution of precision)
-//                        165 = VDOP (Vertical dillution of precision)
-//                            532 = battery input voltage.  In centivolts (5.32v) (After diode, before everything else)
-//                                2/1/0 = 2 new packets with valid fix / 1 new packet with no fix / 0 packets with bad checksums
+//            0 = Age of last ping from ground control
+//             0 = Time validity byte (0-9 = seconds since update.  X=invalid / >10 seconds)
+//              0 = Location validity byte
+//               0 = Altitude validity byte
+//                0 = Speed validity byte
+//                 0 = Course validity byte
+//                   6 = Satellites used for fix
+//                     165 = HDOP (Horrizontal dillution of precision)
+//                         165 = VDOP (Vertical dillution of precision)
+//                             532 = battery input voltage.  In centivolts (5.32v) (After diode, before everything else)
+//                                 2/1/0 = 2 new packets with valid fix / 1 new packet with no fix / 0 packets with bad checksums
 //
 void statusTX() {
   //Static Packet 
@@ -121,6 +135,14 @@ void statusTX() {
   //Current Time
   Serial.print(gps.time.value());
   Serial.print (",");
+
+  //Uplink Status
+  timeSince = millis() - lastRX;
+  if (timeSince < 10000) {
+    Serial.print( timeSince / 1000 );
+  } else {
+    Serial.print("X");
+  }
 
   //Time Status
   if (gps.time.isValid() && gps.time.age() < 10000) {
@@ -198,4 +220,19 @@ void statusTX() {
   //Finally, update last Packet variable
   Serial.println();
   lastStatusTX = millis();
+}
+
+//
+// Processes a command from ground control.
+//
+// Valid packets:
+// P = 'ping' Keeps track of last RX from Ground Control
+//
+void process_command(Byte ch) {
+  if (ch == 'P') {
+    lastRX = millis();
+  }
+  else {
+    //something went wrong?  Bad packet.
+  }
 }
