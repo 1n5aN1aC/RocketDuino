8ec170aeb15581f2d9d08238e29c7143d655c4ed
1n5aN1aC <735325+1n5aN1aC@users.noreply.github.com>
2021-10-22 12:56:29 -0700

    First change for switching to Teensy board

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -4,70 +4,69 @@
 // Purpose: Handles all logic for the RocketDuino
 //
 #include "Arduino.h"
-#include <ESP8266WiFi.h>
 #include <Wire.h>
 #include <BMP280_DEV.h>
 #include <SimpleKalmanFilter.h>
 #include <TinyGPS++.h>
 
 // Pin designations
-#define BUZZER D8
-#define CHUTE1 D6
-#define CHUTE2 D7
+#define BUZZER 17
+#define CHUTE1 8
+#define CHUTE2 7
+#define LED1   4
+#define LED2   3
+
 
 // Barometer Variables
 BMP280_DEV bmp280; // Define barometer object. Basic I2C SCL & SDA
 SimpleKalmanFilter altitudeFilterObject(4, 1, 0.015); //Measurement Uncertainty, Estimation Uncertainty, Noise
 float    temporaryAltitude       = 0;
 float    currentFilteredAltitude = 0;
-uint32_t lastBarometerRead       = 0;
+elapsedMillis lastBarometerRead;
 
 // Mode variables
 #define  FREQUENCY_UPDATE_MODE   250
-uint32_t lastModeUpdate          = 0;
 uint8_t  currentMode             = 0;
+elapsedMillis lastModeUpdate;
 
 // Buzzer Variables
 #define  FREQUENCY_UPDATE_BUZZER 3000
-uint32_t lastBuzzerUpdate        = 0;
+elapsedMillis lastBuzzerUpdate;
 
 // Serial Variables
-#define  SERIALBAUDRATE          57600
-#define  FREQUENCY_UPDATE_SERIAL 350
-uint32_t lastSerialSend          = 0;
-uint32_t lastSerialReceive       = 0;
+#define  LORABAUDRATE            57600
+#define  FREQUENCY_TX_LORA       350
 uint16_t telemetryPacketsSent    = 0;
+elapsedMillis lastLoraTX;
 
 // GPS Variables
 TinyGPSPlus   gps_object;
 TinyGPSCustom gpsStatus(gps_object, "GPGGA", 6);
+#define  GPSBAUDRATE             57600
+bool     GPSforward              = false;
+elapsedMillis lastGPSReceive;
 
-//We only have 1172 bps to work with...
-typedef struct __attribute__((packed)) packet_basic { //[5 BYTES]
-  int16_t currentAlt         : 16;  // -32768..32767    [16 bits]
-  int16_t maxAlt             : 16;  // -32768..32767    [16 bits]
-  uint8_t currentMode        : 8;   // 0..3             [2  bits]
+typedef struct __attribute__((packed)) packet_normal { //Maximum 51 / 58 Bytes
+  int16_t  currentAlt;        //2 bytes
+  int16_t  maxAlt;            //2 bytes
+  uint8_t  currentMode;       //1 byte
+  float    latitude;          //4 bytes
+  float    longitude;         //4 bytes
+  uint16_t gpsAlt;            //2 bytes
+  uint16_t gpsSpeedMPS;       //2 bytes
+  uint8_t  sats;              //1 byte
+  uint8_t  gps_mode;          //1 byte (only 2 bits needed)
+  uint8_t  gps_recent;        //1 byte (only 1 bit needed)
 };
 
-typedef struct __attribute__((packed)) packet_gps {   //[13 BYTES]
-  float    latitude          ;      // float            [32 bits]
-  float    longitude         ;      // float            [32 bits]
-  uint16_t gpsAlt            : 16;  // 0..65535         [16 bits]
-  uint16_t gpsSpeedMPS       : 16;  // 0..65535         [16 bits]
-  uint8_t  sats              : 4;   // 0..15            [4  bits] (4  saved)
-  uint8_t  gps_mode          : 2;   // 0..3             [2  bits] (6  saved)
-  uint8_t  gps_recent        : 1;   // 0..1             [1  bit ] (7  saved)
-  uint8_t  padding           : 1;
-};
-
-typedef struct __attribute__((packed)) packet_extra { //[1 BYTE]
-  int16_t startingAlt       : 16;  // -32768..32767     [16 bits]
+typedef struct __attribute__((packed)) packet_ground { //Maximum 51 / 58 Bytes
+  int16_t startingAlt;        //2 bytes
 };
 
 
 // Saved Values
-int16_t maxAltitudeSeen    = -32768;   //Highest we've been to
-int16_t startingAltitude   = 0;   //what elevation we consider 'ground'
+int16_t  maxAltitudeSeen    = -32768;   //Highest we've been to
+int16_t  startingAltitude   = 0;   //what elevation we consider 'ground'
 uint16_t failsafeAltitude   = 100; //minimum alt we need to reach to arm
 uint8_t  extraDeploymentAlt = 25;  //Extra meters we have to fall below max alt before deployment (to be safe from jiggle)
 
@@ -79,14 +78,17 @@ void setup() {
   pinMode(BUZZER, OUTPUT);
   pinMode(CHUTE1, OUTPUT);
   pinMode(CHUTE2, OUTPUT);
+  pinMode(LED1,   OUTPUT);
+  pinMode(LED2,   OUTPUT);
   
 	//Initialize both serial ports
-  Serial.begin(SERIALBAUDRATE);
+  Serial1.begin(LORABAUDRATE);
+  Serial2.begin(GPSBAUDRATE);
   
   delay(1000);
   
   bmp280.begin(SLEEP_MODE, BMP280_I2C_ALT_ADDR);  // Default initialisation with alternative I2C address (0x76), place the BMP280 into SLEEP_MODE
-  bmp280.setPresOversampling(OVERSAMPLING_X1);    // Options are OVERSAMPLING_SKIP, _X1, _X2, _X4, _X8, _X16
+  bmp280.setPresOversampling(OVERSAMPLING_X2);    // Options are OVERSAMPLING_SKIP, _X1, _X2, _X4, _X8, _X16
   bmp280.setTempOversampling(OVERSAMPLING_X1);    // Options are OVERSAMPLING_SKIP, _X1, _X2, _X4, _X8, _X16
   bmp280.setIIRFilter(IIR_FILTER_OFF);            // Options are IIR_FILTER_OFF, _2, _4, _8, _16
   bmp280.setTimeStandby(TIME_STANDBY_62MS);       // Options are TIME_STANDBY_05MS, _62MS, _125MS, _250MS, _500MS, _1000MS, 2000MS, 4000MS
@@ -104,10 +106,13 @@ void loop() {
   calculate_mode();
   
   //Update Serial / wireless
-  update_serial_send();
+  update_lora_send();
   
   //Handle incoming gps packets
-  update_serial_receive();
+  update_gps_receive();
+
+  //Handle incoming lora packets
+  update_lora_receive();
   
   //Update LEDs / display
   update_buzzer();
@@ -125,19 +130,7 @@ void update_barometer() {
     }
 
     //Take note that we've sucessfully read the altitude:
-    lastBarometerRead = millis();
-
-    //Serial.println();
-    //Serial.print(static_cast<int16_t>(currentFilteredAltitude));
-    //Serial.print(",");
-    //Serial.print(maxAltitudeSeen);
-    //Serial.print(",");
-    //Serial.print(currentMode);
-    //Serial.print(",");
-    //Serial.print(maxAltitudeSeen);
-    //Serial.print(",");
-    //Serial.print(maxAltitudeSeen - extraDeploymentAlt);
-    //Serial.println();
+    lastBarometerRead = 0;
   }
 }
 
@@ -148,8 +141,8 @@ void update_barometer() {
 // Mode 3 = Eject!
 //
 void calculate_mode() {
-  if (millis() - lastModeUpdate > FREQUENCY_UPDATE_MODE) {
-    lastModeUpdate = millis();
+  if (lastModeUpdate > FREQUENCY_UPDATE_MODE) {
+    lastModeUpdate = lastModeUpdate - FREQUENCY_UPDATE_MODE;
     
     // Mode 0 = Boot
     if (currentMode == 0) {
@@ -160,7 +153,7 @@ void calculate_mode() {
         //Serial.println("Mode 0->1");
       }
       //Also, if we haven't read the barometer recently, abort:
-      if (millis() > 5000 && millis() - lastBarometerRead > 1000) {
+      if (millis() > 5000 && lastBarometerRead > 1000) {
         tone(BUZZER, 4000);
         delay(90000);
       }
@@ -203,54 +196,59 @@ void calculate_mode() {
   }
 }
 
-void update_serial_receive() {
-  while (Serial.available() > 0) {
-    gps_object.encode(Serial.read());
-    lastSerialReceive = millis();
-  }
-}
-
-void update_buzzer() {
-  if (millis() - lastBuzzerUpdate > FREQUENCY_UPDATE_BUZZER) {
-    lastBuzzerUpdate = millis();
-    if (currentMode == 0) {
-      tone(BUZZER, 2000);  // Send 2KHz sound signal...
-      delay(75);          // ...for 1 sec
-      noTone(BUZZER);      // Stop sound...
-      delay(50);
-      tone(BUZZER, 4000);  // Send 4KHz sound signal...
-      delay(75);          // ...for 1 sec
-      noTone(BUZZER);      // Stop sound...
-      delay(50);
-      tone(BUZZER, 2000);  // Send 2KHz sound signal...
-      delay(75);          // ...for 1 sec
-      noTone(BUZZER);      // Stop sound...
-    } else if (currentMode == 1) {
-      tone(BUZZER, 4000);  // Send 4KHz sound signal...
-      delay(25);          // ...for 1 sec
-      noTone(BUZZER);      // Stop sound...
-    } else if (currentMode == 3) {
-      tone(BUZZER, 4000);  // Send 4KHz sound signal...
-      delay(100);          // ...for 1 sec
-      noTone(BUZZER);      // Stop sound...
-      delay(100);
-      tone(BUZZER, 4000);  // Send 4KHz sound signal...
-      delay(100);          // ...for 1 sec
-      noTone(BUZZER);      // Stop sound...
+//Process any incoming bytes from the GPS
+void update_gps_receive() {
+  while (Serial2.available() > 0) {
+    byte b = Serial2.read();
+    gps_object.encode(b);
+    lastGPSReceive = 0;
+    
+    //If GPSforward is enabled, then pass all GPSdata through lora to the basestation
+    if (GPSforward) {
+      Serial1.write(b);
     }
   }
 }
 
-void update_serial_send() {
-  if (millis() - lastSerialSend > FREQUENCY_UPDATE_SERIAL) {
-    lastSerialSend = millis();
+void update_buzzer() {
+  if (lastBuzzerUpdate > FREQUENCY_UPDATE_BUZZER) {
+    lastBuzzerUpdate = lastBuzzerUpdate - FREQUENCY_UPDATE_BUZZER;
+    if (currentMode == 0) {
+      tone(BUZZER, 2000);  // Send 2KHz sound signal...
+      delay(75);          // ...for 75ms
+      noTone(BUZZER);      // Stop sound...
+      delay(50);
+      tone(BUZZER, 4000);  // Send 4KHz sound signal...
+      delay(75);          // ...for 75ms
+      noTone(BUZZER);      // Stop sound...
+      delay(50);
+      tone(BUZZER, 2000, 25);  // Send 2KHz sound signal for 75ms
+    } else if (currentMode == 1) {
+      tone(BUZZER, 4000, 25);  // Send 4KHz sound signal for 25ms
+    } else if (currentMode == 3) {
+      tone(BUZZER, 4000);  // Send 4KHz sound signal...
+      delay(100);          // ...for 100ms
+      noTone(BUZZER);      // Stop sound...
+      delay(100);
+      tone(BUZZER, 4000, 100);  // Send 4KHz sound signafor 100ms
+    }
+  }
+}
 
-    if (telemetryPacketsSent % 51 == 0) {
-      send_packet_extra();
-    } else if (telemetryPacketsSent % 5 == 0) {
-      send_packet_gps();
+void update_lora_receive() {
+  while (Serial1.available() > 0) {
+    byte b = Serial2.read();
+  }
+}
+
+void update_lora_send() {
+  if (lastLoraTX > FREQUENCY_TX_LORA) {
+    lastLoraTX = lastLoraTX - FREQUENCY_TX_LORA;
+
+    if (telemetryPacketsSent % 5 == 0) {
+      send_packet_ground();
     } else {
-      send_packet_basic();
+      send_packet_normal();
     }
 
     //Increment counter for next time
@@ -258,12 +256,7 @@ void update_serial_send() {
   }
 }
 
-void send_packet_basic() {
-  packet_basic packet = {static_cast<int16_t>(currentFilteredAltitude), maxAltitudeSeen, currentMode};
-  Serial.write(reinterpret_cast<char*>(&packet), sizeof packet);
-}
-
-void send_packet_gps() {
+void send_packet_normal() {
   //When was last gps data?  If it was valid, but longer than a couple seconds, override the gps info pkt:
   uint8_t gps_mode;
   if (atoi(gpsStatus.value()) != 0 && gps_object.location.age() > 1500) {
@@ -274,15 +267,15 @@ void send_packet_gps() {
 
   //When was the last time we got GPS packets?
   uint8_t gps_recent = 0;
-  if (millis() - lastSerialReceive < 1000) {
+  if (lastGPSReceive < 1000) {
     gps_recent = 1;
   }
   
-  packet_gps packet = {gps_object.location.lat(), gps_object.location.lng(), gps_object.altitude.meters(), gps_object.speed.mps(), gps_object.satellites.value(), gps_mode, gps_recent, 0};
-  Serial.write(reinterpret_cast<char*>(&packet), sizeof packet);
+  packet_normal packet = {static_cast<int16_t>(currentFilteredAltitude), maxAltitudeSeen, currentMode, gps_object.location.lat(), gps_object.location.lng(), gps_object.altitude.meters(), gps_object.speed.mps(), gps_object.satellites.value(), gps_mode, gps_recent};
+  Serial1.write(reinterpret_cast<char*>(&packet), sizeof packet);
 }
 
-void send_packet_extra() {
-	packet_extra packet = {startingAltitude};
-  Serial.write(reinterpret_cast<char*>(&packet), sizeof packet);
+void send_packet_ground() {
+  packet_ground packet = {startingAltitude};
+  Serial1.write(reinterpret_cast<char*>(&packet), sizeof packet);
 }
